d5ee95d1d339dadd4a8f111f98d25938f16bf8fa
Danny200523 <danicrack0709@gmail.com>
2025-09-22 08:05:45 -0500

    feat: :rockt: first endpoint

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-const express = require("express")
+const express = require('express');
+const app = express();
 
-express = express.json()
+app.get('/api/health', (_req, res) => res.json({ ok: true }));
