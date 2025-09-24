b4622e0aae5b50297a948341b9023dd5b7c8da73
danicrack0709 <danicrack0709@gmail.com>
2025-09-24 21:22:05 +0000

    feat: :rocket: repair port

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
 const app = express();
+const PORT = process.env.PORT
 
 app.get('/api/health', (_req, res) => res.json({ ok: true }));
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
 export default app;
 
