749d476358c75d8b10f838ed2a9e6c60f1dfab08
Danny200523 <danicrack0709@gmail.com>
2025-09-27 11:27:07 -0500

    fix: :fire: repair names of funtions

--- Controllers/resenaController.js
+++ Controllers/resenaController.js
@@ -4,27 +4,27 @@ const resenaModel = new resena()
 
 export class resenaController{
     async createResena(id_usuario,id_pelicula,comentario,calificacion){
-        const result = await resenaModel.createResena(id_usuario,id_pelicula,comentario,calificacion)
+        const result = await resenaModel.create(id_usuario,id_pelicula,comentario,calificacion)
         return result
     }
 
     async getAllResenas(){
-        const result = await resenaModel.getAllResenas()
+        const result = await resenaModel.getAll()
         return result
     }
 
     async getResenaByMovie(id){
-        const result = await resenaModel.getResenaByMovie(id)
+        const result = await resenaModel.getByMovie(id)
         return result
     }
 
     async updateResena(id,req){
-        const result = await resenaModel.updateResena(id,req)
+        const result = await resenaModel.update(id,req)
         return result
     }
 
     async deleteResena(id){
-        const result = await resenaModel.deleteResena(id)
+        const result = await resenaModel.delete(id)
         return result
     }
 }
