2655eb7a17e130b760a3de5df82459d251ba1b71
Danny200523 <danicrack0709@gmail.com>
2025-09-27 23:55:21 -0500

    fix: :wrench: deleting this.

--- Controllers/reaccionController.js
+++ Controllers/reaccionController.js
@@ -1,27 +1,25 @@
 import { reaccion } from "../Models/reaccionModel.js";
+const reaccion = new reaccion()
+
 
 export class reaccionController{
-    constructor(){
-        this.reaccion = new reaccion()
-    }
-
     async createReaccion(req){
-        const result = await this.reaccion.create(req)
+        const result = await reaccion.create(req)
         return result
     }
 
     async listReacciones(){
-        const result = await this.reaccion.list()
+        const result = await reaccion.list()
         return result
     }
     
     async updateReacciones(id,req){
-        const result = await this.reaccion.update(id,req)
+        const result = await reaccion.update(id,req)
         return result
     }
 
     async deleteReaccion(id){
-        const result = await this.reaccion.delete(id)
+        const result = await reaccion.delete(id)
         return result
     }
 }
